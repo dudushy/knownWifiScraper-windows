5640ea576872c15848520cff4bab83702c79e03c
dudushy <eduardotalarico1337@gmail.com>
2022-04-11 08:49:27 -0300

    minor tweaks

--- main.py
+++ main.py
@@ -1,68 +1,68 @@
 # sourcery skip: convert-to-enumerate, for-append-to-extend, list-comprehension, move-assign-in-block, use-dict-items
-#imports
+# Imports
 import subprocess
 import os
 import time
 
-#main
+# Main
 while True:
-    # clear screen
+    # Clear screen
     os.system('cls')
 
-    # reset vars
+    # Reset vars
     profiles = []
     passwords = {}
     count = 0
 
-    # grab all profiles (raw data)
+    # Grab all profiles (raw data)
     profiles_raw = subprocess.check_output(["netsh", "wlan", "show", "profiles"], encoding="cp858")
     
-    # format profiles into list
+    # Format profiles into list
     for line in profiles_raw.split("\n"):
         if "Profile     :" in line:
             profiles.append(line[(line.find(":")) + 2:])
 
-    # print info
+    # Print info
     print(f"""
 -----] Wifi Key Scraper [-----
 | [!] ({len(profiles)}) Profiles found""")
 
-    # wait 2 second
-    time.sleep(2)
+    # Wait 1 seconds
+    time.sleep(1)
 
-    # grab each key
+    # Grab each key
     for profile in profiles:
         print(f'|\n| [! INIT] Profile: "{profile}"')
-        # try execute code
+        # Try execute code
         try:
             password_raw = subprocess.check_output(["netsh", "wlan", "show", "profile", f'name={profile.replace(" ", "*")}', "key=clear"], shell=True, encoding="cp858")
             for line in password_raw.split("\n"):
                 if "Key Content            :" in line:
-                    passwords[f"profile{count}"] = {
+                    passwords[f"profile{count + 1}"] = {
                         "status": "SUCESS",
                         "id": profile,
                         "password": line[(line.find(":")) + 2:]
                         }
                     print(f'|\n| [! DONE] Profile: "{profile}" // Password: "{line[(line.find(":")) + 2:]}"')
-        # set password to null if fail
+        # Set password to null if fail
         except Exception as e:
             print(f'|\n| [! ERROR] Profile: "{profile}" // FAIL')
-            passwords[f"profile{count}"] = {
+            passwords[f"profile{count + 1}"] = {
                         "status": "ERROR",
                         "id": profile,
                         "password": "null"
                         }
         count += 1
 
-    # save output text
-    with open("output.txt", "w") as file:
+    # Save output text
+    with open(os.path.join(os.path.dirname(__file__), "output.txt"), "w") as file:
         for item in passwords:
             file.write(f"[{item}/{passwords[item]['status']}] {passwords[item]['id']} <|> {passwords[item]['password']}\n")
         print("|\n| [!] 'output.txt' saved")
     
-    # repeat loop?
-    if input("|\n-----] Execute again? (y/n) [-----\n>> ") == "n":
+    # Repeat loop?
+    if input("|\n-----] Execute again? (y/n) [-----\n>> ").lower() == "n":
         break
 
-#end
+# End
 os.system('pause')
